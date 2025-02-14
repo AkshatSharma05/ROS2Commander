9beaae60ca655debdc1eb1530469c17f669b41c7
AkshatSharma05 <d06akshat@gmail.com>
2025-02-15 01:42:13 +0530

    implemented velocity change and added icon

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,14 @@ class Home extends StatelessWidget {
   @override
   
   Widget build(BuildContext context) {
+  double linearVel = 1.0;
+  double angularVel = 5.0;
 
   late ROSBridgeClient rosClient;
   String selectedTopic = "/cmd_vel";
+  String ipAddress = "192.168.1.1";
 
-  rosClient = ROSBridgeClient("192.168.1.87"); // Change to your ROS2 device IP
+  rosClient = ROSBridgeClient(ipAddress); // Change to your ROS2 device IP
 
     final List<IconData> icons = [
       Icons.arrow_upward_rounded,
@@ -32,23 +35,27 @@ class Home extends StatelessWidget {
     ];
 
     final List<Map<String, double>> velocities = [
-      {"linearX": 1.0, "angularZ": -1.0}, // Forward left
-      {"linearX": 1.0, "angularZ": 0.0}, // Forward
-      {"linearX": 1.0, "angularZ": 1.0}, // Forward right
-      {"linearX": 0.0, "angularZ": -1.0}, // Rotate left
+      {"linearX": linearVel, "angularZ": -angularVel}, // Forward left
+      {"linearX": linearVel, "angularZ": 0.0}, // Forward
+      {"linearX": linearVel, "angularZ": angularVel}, // Forward right
+      {"linearX": 0.0, "angularZ": -angularVel}, // Rotate left
       {"linearX": 0.0, "angularZ": 0.0}, // Stop (center button)
-      {"linearX": 0.0, "angularZ": 1.0}, // Rotate right
-      {"linearX": -1.0, "angularZ": -1.0}, // Backward left
-      {"linearX": -1.0, "angularZ": 0.0}, // Backward
-      {"linearX": -1.0, "angularZ": 1.0}, // Backward right
+      {"linearX": 0.0, "angularZ": angularVel}, // Rotate right
+      {"linearX": -linearVel, "angularZ": -angularVel}, // Backward left
+      {"linearX": -linearVel, "angularZ": 0.0}, // Backward
+      {"linearX": -linearVel, "angularZ": angularVel}, // Backward right
     ];
 
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 41, 41, 41),
       appBar: AppBar(
-        title: Text('ROS2 Commander'),
+        backgroundColor: Colors.blueAccent,
+        foregroundColor: Colors.white,
+        title: Text('ROS2Commander'),
         centerTitle: true,
       ),
-      body: Padding(
+      body: 
+      Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
@@ -73,6 +80,7 @@ class Home extends StatelessWidget {
                           foregroundColor: Colors.white,
                         ),
                         child: Icon(
+                          color: Colors.black,
                           icons[index],
                           size: 40.0, // Increase the icon size
                         ),
@@ -82,16 +90,92 @@ class Home extends StatelessWidget {
                 ),
               ),
             ),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter Topic',
-              ),
-              onSubmitted: (String value) {
-                selectedTopic = value;
-                print('Selected topic: $selectedTopic');
-              },
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 2), // Set the border color when enabled
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green, width: 2), // Set the border color when focused
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: 'Linear Velocity',
+                      
+                    ),
+                    onSubmitted: (String value) {
+                      linearVel = double.parse(value);
+                    },
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue, width: 2), // Set the border color when enabled
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green, width: 2), // Set the border color when focused
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: 'Angular Velocity',
+                    ),
+                    onSubmitted: (String value) {
+                      angularVel = double.parse(value);
+                    },
+                  ),
+                ),
+              ],
             ),
+            SizedBox(height: 10),
+            Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2), // Set the border color when enabled
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 2), // Set the border color when focused
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: 'Enter Topic',
+                    ),
+                    onSubmitted: (String value) {
+                      selectedTopic = value;
+                    },
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue, width: 2), // Set the border color when enabled
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.green, width: 2), // Set the border color when focused
+                      ),
+                      fillColor: Colors.white,
+                      filled: true,
+                      labelText: 'Enter IP Address',
+                    ),
+                    onSubmitted: (String value) {
+                      rosClient.close();
+                      ipAddress = value;
+                      rosClient = ROSBridgeClient(ipAddress);
+                    },
+                  ),
+                ],
+              ),
           ],
         ),
       ),
